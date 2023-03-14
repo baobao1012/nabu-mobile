ba52a2d66343496eb0864ce4e03faf208acb71cd
julyle1012 <ngocbaole902@gmail.com>
2023-03-15 01:11:37 +0700

    forgot password

---
+++ Nabu Mobile/Pages/Account/SignUp.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyRazorPage.common;
+using MyRazorPage.Models;
+
+namespace MyRazorPage.Pages.Account
+{
+    public class SignUpModel : PageModel
+    {
+        
+        private readonly PRN221_DBContext prn221DBContext;
+        private readonly Random _random = new();
+        private readonly int lENGTH_CUSTOMER_ID = 5;
+        private readonly CommonRole commonRole = new();
+
+        public SignUpModel(PRN221_DBContext prn221DBContext) => this.prn221DBContext = prn221DBContext;
+
+        //Khai báo 2 thuộc tính đại diện cho 2 đối tượng : Account, Customer
+        [BindProperty]
+        public Customer customer { get;set; }
+        [BindProperty]
+        public Models.Account account { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<ActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var accountInDB = await prn221DBContext
+                    .Accounts
+                    .SingleOrDefaultAsync(x => x.Email.Equals(account.Email));
+                if (accountInDB == null)
+                {
+                    var _customer = new Customer
+                    {
+                        CustomerId = generatedCustomerId(),
+                        CompanyName = customer.CompanyName,
+                        ContactName = customer.ContactName,
+                        ContactTitle = customer.ContactTitle,
+                        Address = customer.Address
+                    };
+
+                    var _account = new Models.Account
+                    {
+                        Email = account.Email,
+                        Password = account.Password,
+                        CustomerId = _customer.CustomerId,
+                        Role = commonRole.CUSTOMER_ROLE
+                    };
+
+                    await prn221DBContext.Customers.AddAsync(_customer);
+                    await prn221DBContext.Accounts.AddAsync(_account);
+                    await prn221DBContext.SaveChangesAsync();
+                    return RedirectToPage("/index");
+                }
+                    ViewData["message"] = "This account exists";
+                    return Page();
+            } else
+            {
+                return Page();
+            }
+        }
+
+        private string generatedCustomerId()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, lENGTH_CUSTOMER_ID)
+                .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+       
+    }
+}
