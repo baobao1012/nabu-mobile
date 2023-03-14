e9659140f9d8370df04f25b6b7424ad434f5b40a
julyle1012 <ngocbaole902@gmail.com>
2023-03-14 21:47:03 +0700

    update role

---
+++ nabu-mobile/nabu-mobile/common/CommonRole.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace MyRazorPage.common
+{
+    public class CommonRole
+    {
+        public int CUSTOMER_ROLE = 2;
+        public int EMPLOYEE_ROLE = 1;
+    }
+}
